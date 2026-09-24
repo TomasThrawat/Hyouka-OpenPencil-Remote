8cdf8f2b621e99a0d978005882609dad315946d7
TomasThrawat <tomastharwat873@gmail.com>
2026-09-24 19:46:08 +0300

    fix: use supported tsdown zod externalization

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -34,9 +34,8 @@ export default defineConfig({
   sourcemap: true,
   clean: true,
   outDir: './dist',
-  external: ['zod'],
   deps: {
-    neverBundle: [...Object.keys(packageJSON.dependencies ?? {}), /^node:/],
+    neverBundle: [...new Set([...Object.keys(packageJSON.dependencies ?? {}), 'zod', /^node:/])],
     onlyBundle: false
   }
 })
